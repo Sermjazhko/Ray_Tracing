d99aeddff4bf98f93e51597b2e453ac8e8113cb7
Elizaveta Sermjazhko <43463670+Sermjazhko@users.noreply.github.com>
2021-05-17 09:34:18 +0300

    New

---
+++ WindowsFormsApplication6/WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        GLgraphics glgraphics = new GLgraphics();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            while (glControl1.IsIdle)
+                glControl1.Refresh();
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            glgraphics.Update();
+            glControl1.SwapBuffers();
+        }
+
+        private void glControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // преобразование координат текущего положения мыши в расстояние от центра изображения до текущего положения мыши
+            float widthCoef = (e.X - glControl1.Width * 0.5f) / (float)glControl1.Width;
+            float heightCoef = (-e.Y + glControl1.Height * 0.5f) / (float)glControl1.Height;
+            glgraphics.latitude = heightCoef * 180;
+            glgraphics.longitude = widthCoef * 360;
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            glgraphics.Setup(glControl1.Width, glControl1.Height);
+            Application.Idle += Application_Idle;
+        }
+    }
+}
---
+++ WindowsFormsApplication6/WindowsFormsApplication6/GLgraphics.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.IO;
+
+namespace WindowsFormsApplication6
+{
+    class GLgraphics
+    {
+        Vector3 cameraPosition = new Vector3(0, 0, 10);
+        Vector3 cameraDirecton = new Vector3(0, 0, 0);
+        Vector3 cameraUp = new Vector3(0, 0, 1);
+
+        public float latitude = 47.98f;
+        public float longitude = 60.41f;
+        public float radius = 7.385f;
+
+
+        public List<int> texturesIDs = new List<int>();
+
+        int BasicProgramID;
+        int BasicVertexShader;
+        int BasicFragmentShader;
+        //int vaoHandle;
+
+        public void Setup(int width, int height)
+        {
+            string glversion = GL.GetString(StringName.Version);
+            string glslversion = GL.GetString(StringName.ShadingLanguageVersion);
+
+            Console.WriteLine(glversion);
+            Console.WriteLine(glslversion);
+
+
+            GL.ClearColor(Color.DarkGray);
+            GL.ShadeModel(ShadingModel.Smooth);
+            GL.Enable(EnableCap.DepthTest);
+
+            Matrix4 perspectiveMat = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, width / (float)height, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspectiveMat);
+
+            InitShaders();
+
+        }
+
+        public void Update()
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            Matrix4 viewMat = Matrix4.LookAt(cameraPosition, cameraDirecton, cameraUp);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref viewMat);
+            Render();
+        }
+
+        public void Render()
+        {
+            Draw();
+        }
+
+        void loadShader(String filename, ShaderType type, int program, out int address)
+        {
+
+            address = GL.CreateShader(type);
+            using (System.IO.StreamReader sr = new StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
+        }
+
+        private void InitShaders()
+        {
+            // создание объекта программы 
+            BasicProgramID = GL.CreateProgram();
+            loadShader("..\\..\\RayShaders\\RayTracing.vs", ShaderType.VertexShader, BasicProgramID, out BasicVertexShader);
+            loadShader("..\\..\\RayShaders\\RayTracing.fs", ShaderType.FragmentShader, BasicProgramID, out BasicFragmentShader);
+            
+            //Компановка программы
+            GL.LinkProgram(BasicProgramID);
+
+            // Проверить успех компановки
+            int status = 0;
+            GL.GetProgram(BasicProgramID, GetProgramParameterName.LinkStatus, out status);
+
+            Console.WriteLine(GL.GetProgramInfoLog(BasicProgramID));
+
+            /*float[] positionData = { -0.8f, -0.8f, 0.0f, 0.8f, -0.8f, 0.0f, 0.0f, 0.8f, 0.0f };
+            float[] colorData = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };*/
+            /*float[] positionData = { -1, -1, 0, -1, 1, 0, 1, 1, 0, 1, -1, 0 };
+            float[] colorData = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f };
+
+            int[] vboHandlers = new int[2];
+
+            GL.GenBuffers(2, vboHandlers);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboHandlers[0]);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(sizeof(float) * positionData.Length), positionData, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboHandlers[1]);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(sizeof(float) * colorData.Length), colorData, BufferUsageHint.StaticDraw);
+
+            vaoHandle = GL.GenVertexArray();
+            GL.BindVertexArray(vaoHandle);
+
+            GL.EnableVertexAttribArray(0);
+            GL.EnableVertexAttribArray(1);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboHandlers[0]);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboHandlers[1]);
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 0, 0);*/
+
+        }
+
+        public void Draw()
+        {
+            GL.UseProgram(BasicProgramID);
+            GL.Begin(PrimitiveType.Quads);
+            GL.Vertex3(-1, -1, 0);
+            GL.Vertex3(-1, 1, 0);
+            GL.Vertex3(1, 1, 0);
+            GL.Vertex3(1, -1, 0);
+            GL.End();
+            GL.UseProgram(0);
+        }
+    }
+}
